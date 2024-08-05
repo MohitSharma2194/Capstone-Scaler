713d48b1e05598b913c9e6859bd36e3fed7c0ef8
Mohit Sharma <er.mohitsharma@outlook.com>
2024-08-05 18:41:37 +0000

    Ticket code check in

---
+++ src/main/java/com/scaler/bookmyshow/repositories/AuditoriumRepository.java
@@ -0,0 +1,7 @@
+package com.scaler.bookmyshow.repositories;
+
+import com.scaler.bookmyshow.models.Auditorium;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AuditoriumRepository extends JpaRepository<Auditorium, Long> {
+}
